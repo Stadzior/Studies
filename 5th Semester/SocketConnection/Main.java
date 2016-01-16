d7b1f316f12a08bd2bdec9bc7a4bd9cf78097f81
Stadzior <Stadzior@gmail.com>
2016-01-16 15:14:32 +0100

    Teacher's objections resolved.

--- 5th Semester/SocketConnection/Main.java
+++ 5th Semester/SocketConnection/Main.java
@@ -1,16 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.sql.Time;
+import java.net.URL;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            String Url = args[0];
-            String HostName = Url.substring(Url.indexOf(":")+3,Url.length());
+            URL Url = new URL(args[0]);
+            String HostName = Url.getHost() +Url.getPath();
             InetAddress HostAddress = InetAddress.getByName(HostName);
-            Socket httpSocket = new Socket(HostAddress, 80);
+            Integer Port = Url.getPort() == -1 ? Url.getDefaultPort() : Url.getPort();
+            Socket httpSocket = new Socket(HostAddress,Port);
             double timeElapsed = System.currentTimeMillis();
             PrintWriter pw = new PrintWriter(httpSocket.getOutputStream());
             pw.print("GET / HTTP/1.0\r\n");
@@ -18,13 +21,32 @@ public class Main {
             pw.print("\r\n");
             pw.flush();
             timeElapsed = System.currentTimeMillis() - timeElapsed;
-            String RemoteAddress = httpSocket.getRemoteSocketAddress().toString();
-            Integer LocalTCPPort = httpSocket.getLocalPort();
-            Integer SizeOfReply = httpSocket.getReceiveBufferSize();
-            System.out.println("Remote address: "+RemoteAddress);
-            System.out.println("Local TCP port: "+LocalTCPPort);
-            System.out.println("Size of reply: "+SizeOfReply);
-            System.out.println("Time elapsed: "+timeElapsed);
+            StringBuilder out = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(httpSocket.getInputStream()));
+            String line;
+            if((line = br.readLine()) != null && (line.contains("200"))) {
+                Integer SizeOfReply = 0;
+                while ((line = br.readLine()) != null && line.length() > 0) {
+
+                    if(line.contains("Content-Length")) {
+                        SizeOfReply = Integer.parseInt(line.substring(line.indexOf(":")+1).trim());
+                    }
+                }
+                while((line = br.readLine()) != null) {
+                    out.append(line + "\n");
+                }
+                System.out.println(out);
+                System.out.println("-----------------------------");
+                String RemoteAddress = httpSocket.getRemoteSocketAddress().toString();
+                Integer LocalTCPPort = httpSocket.getLocalPort();
+                System.out.println("Remote address: " + RemoteAddress);
+                System.out.println("Local TCP port: " + LocalTCPPort);
+                System.out.println("Size of Reply: " + SizeOfReply);
+                System.out.println("Time elapsed: " + timeElapsed);
+            }
+            else {
+                throw new Exception("Returned code different from 200.");
+            }
         }
         catch (Exception e){
             System.out.println(e.getMessage());
