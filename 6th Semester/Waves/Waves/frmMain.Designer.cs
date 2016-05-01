2120383fb7c45bb65bb9c676d1b91b13f7715330
Stadzior <Stadzior@gmail.com>
2016-05-01 16:32:30 +0200

    Waves exercise.

---
+++ 6th Semester/Waves/Waves/frmMain.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Waves
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxMain
+            // 
+            this.pictureBoxMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxMain.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxMain.MinimumSize = new System.Drawing.Size(10, 10);
+            this.pictureBoxMain.Name = "pictureBoxMain";
+            this.pictureBoxMain.Size = new System.Drawing.Size(650, 390);
+            this.pictureBoxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxMain.TabIndex = 0;
+            this.pictureBoxMain.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 390);
+            this.Controls.Add(this.pictureBoxMain);
+            this.MinimumSize = new System.Drawing.Size(10, 10);
+            this.Name = "frmMain";
+            this.Text = "0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMain;
+    }
+}
+
---
+++ 6th Semester/Waves/Waves/frmMain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Waves
+{
+    public partial class frmMain : Form
+    {
+        delegate void SetTextCallback(string text);
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        Task _projection;
+        CancellationTokenSource _cancellationTokenSource;
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            _projection = Task.Run(()=>StartWaves(),_cancellationTokenSource.Token);
+        }
+
+        private void StartWaves()
+        {
+            double t = 0.0;
+            double i = 0;
+            while (true)
+            {
+                RefreshWaves(t);
+                i++;
+                SetText(i.ToString());
+                t += 0.1;
+            }
+        }
+
+        private void RefreshWaves(double t)
+        {
+            Bitmap image = new Bitmap(pictureBoxMain.Width, pictureBoxMain.Height);
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    int r = (int)((0.5+0.5*Math.Sin(((Math.Pow(x-pictureBoxMain.Width/2,2)+Math.Pow(y-pictureBoxMain.Height/2,2))/1000)+t))*255);
+                    Color fadedRed = Color.FromArgb(r, 0, 0);
+                    image.SetPixel(x, y,fadedRed);
+                }
+            }
+            pictureBoxMain.Image = image;
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _cancellationTokenSource.Cancel();
+        }
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (InvokeRequired)
+            {
+                SetTextCallback callback = new SetTextCallback(SetText);
+                Invoke(callback, new object[] { text });
+            }
+            else
+            {
+                Text = text;
+            }
+        }
+    }
+}
